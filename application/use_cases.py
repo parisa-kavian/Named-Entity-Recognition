f54ff9808daef04d8cfb888d18a7ef398a6dfc86
Parisa Kavianpour <142240782+parisa-kavian@users.noreply.github.com>
2026-06-03 18:21:07 +0330

    Update use_cases.py

--- application/use_cases.py
+++ application/use_cases.py
@@ -1,28 +1,17 @@
-
 from infrastructure.data_loader import load_dataset, tokenize_and_align_labels
-from infrastructure.model_utils import get_tokenizer, get_model, train_model, save_model_and_tokenizer
-from infrastructure.metric_utils import load_metric, compute_metrics
+from infrastructure.model_utils import get_tokenizer, get_model, build_trainer, save_model_and_tokenizer
+
 
 def train_ner_model():
-
     data = load_dataset()
-
+    label_list = data["train"].features["ner_tags"].feature.names
 
     tokenizer = get_tokenizer()
-
-
     tokenized_datasets = data.map(tokenize_and_align_labels, batched=True)
 
-
-    model = get_model()
-
-  
-    trainer = train_model(model, tokenizer, tokenized_datasets)
-
- 
+    model = get_model(label_list)
+    trainer = build_trainer(model, tokenizer, tokenized_datasets, label_list)
     trainer.train()
 
     save_model_and_tokenizer(model, tokenizer)
-
     return model, tokenizer
-
